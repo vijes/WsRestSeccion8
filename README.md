350a1518f79877814f83f4cffe9a9262243ed4c4
Victor Jaramillo <vjaramillo@ec.krugercorp.com>
2020-03-03 10:42:24 -0500

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+// Seccion 8 Creación de REST WS
+
+En esta sección es para configurar web service y vinculacion con mongodb.
+
+```
+npm install
+```
---
+++ server/server.js
@@ -0,0 +1,53 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Servicios
+app.get('/usuario', function(req, res) {
+    res.json('get usuario');
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es requerido"
+        });
+    } else {
+
+        res.json({
+            usuario: body
+        });
+    }
+
+});
+
+app.put('/usuario/:idRes', function(req, res) {
+
+    let id = req.params.idRes;
+    res.json({
+        id
+
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario');
+});
+
+// Puerto del servidor.
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando puerto ${process.env.PORT}`);
+
+});
