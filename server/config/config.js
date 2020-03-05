fcdcaa2bd6d105b1ff6b8e3ad0768415b8745629
Victor Jaramillo <vjaramillo@ec.krugercorp.com>
2020-03-05 11:45:32 -0500

    Variable de entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://vijes:OyLAcjP11tF2rfQF@cluster0-uje6t.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URLDB;
 }
 
 process.env.URLDB = urlDB;
