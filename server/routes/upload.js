4f7a0fcc08ce882569e4dbd25a23f95bfff2ebb7
Victor Jaramillo E <victor87jaramillo@gmail.com>
2020-03-26 22:45:47 -0500

    Validación para subir archivos con upload file

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -4,3 +4,46 @@ const app = express();
 
 // default options
 app.use(fileUpload());
+
+app.put('/upload', function(req, res) {
+
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'No se selecciono ningun archivo'
+            }
+        });
+    }
+
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+    let archivo = req.files.archivo;
+    let extensionArchivo = archivo.name.split('.')[1];
+    console.log(`Archivo a subir: ${archivo.name}`);
+    console.log(`Extension archivo: ${extensionArchivo}`);
+
+    if (extensionesValidas.indexOf(extensionArchivo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: `Extensiones permitidas ${extensionesValidas}`,
+                ext: extensionArchivo
+            }
+        });
+    }
+    archivo.mv(`recursos/imagenes/${archivo.name}`, (err) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            message: 'Se subio correctamente el archivo'
+        });
+    });
+});
+
+module.exports = app;
